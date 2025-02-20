da312958d77c45f1a2077a0d70af7dc34559ba26
ash14feb <ashwaq.cs@gmail.com>
2025-02-20 12:09:58 +0530

    Add files via upload

---
+++ script_DXG.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Run as administrator to change system date and time
+:: Set Date to January 10, 2025 (Change the year if needed)
+date 10-01-2025
+:: Set Time to 10:00 AM (Change as needed)
+time 10:00
